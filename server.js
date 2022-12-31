30069dcfbaaa318f0bf00be8f8cb1b975fafde9f
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2022-12-31 13:40:22 +0100

    updte

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 80;
+const PORT = 8800;
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
