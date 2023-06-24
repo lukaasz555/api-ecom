9bf3fe4f2e64e8f79b1c046f4a2228fa87db9a67
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-06-24 17:30:44 +0200

    Admin access to some order routes

--- controllers/orders/putOrder.ts
+++ controllers/orders/putOrder.ts
@@ -1,16 +1,22 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
+import { UserAuthenticatedReq } from '../../interfaces/UserAuthenticatedReq';
 import { OrderModel } from '../../interfaces/OrderModel';
+import { UserRolesEnum } from '../../enums/UserRolesEnum';
 const Order = require('../../schemas/Order');
 
-exports.putOrder = async (req: Request, res: Response) => {
-	try {
-		const order: OrderModel = await Order.findByIdAndUpdate(req.body.id, {
-			$set: {
-				status: req.body.status,
-			},
-		});
-		res.status(200).json(order);
-	} catch (err) {
-		console.log(err);
+exports.putOrder = async (req: UserAuthenticatedReq, res: Response) => {
+	if (req.user && req.user.role === UserRolesEnum.Admin) {
+		try {
+			const order: OrderModel = await Order.findByIdAndUpdate(req.body.id, {
+				$set: {
+					status: req.body.status,
+				},
+			});
+			res.status(200).json(order);
+		} catch (err) {
+			console.log(err);
+		}
+	} else {
+		res.status(403).json('Forbidden');
 	}
 };
